1c7a7f1c567256bd562e3ff0c940cd6df05b618d
Monik Pamecha <monik@etiole.com>
2015-04-26 17:17:37 +0530

    Circle using Polar Coordinates

--- circle-using-polar.cpp
+++ circle-using-polar.cpp
@@ -1,46 +1,24 @@
+//circle using parametric form
 #include<stdio.h>
 #include<conio.h>
 #include<graphics.h>
 #include<math.h>
+
 void main() {
-
+	
 	int gd=DETECT,gm;
-
-	long x,y,x1,y1,r,p;
-
-	printf("Enter the radius\n");
-
-	scanf("%ld",&r);
-
+	int r,i=0;
+	clrscr();
 	initgraph(&gd,&gm,"");
-
-	setcolor(WHITE);
-	//coordinate axes
-	line(0,250,600,250);
-	line(300,0,300,600);
-
-	x=-r;
-	y=0;
-
-	while(abs(x)>0)
+	printf("Enter the center coordinates");
+	scanf("%d%d",&xc,&yc);
+	printf("Enter radius");
+	scanf("%d",&r);
+	for(i=0; i<360; i++)
 	{
-		putpixel(300+x,250+y,RED);
-		putpixel(300-x,250+y,RED);
-		putpixel(300+x,250-y,RED);
-		putpixel(300-x,250-y,RED);
-	       //	printf("%ld %ld %ld\n",r,x,y);
-		//long val =0,r2=0,x2=0;
-		//r2 = r*r;
-		//x2 = x*x;
-		//val = r2-x2;
-	       //	printf("%ld\n",r2);
-		y=sqrt(r*r-x*x);
-		x++;
+		putpixel(xc+r*cos(i*3.14/180),yc+r*sin(i*3.14/180),RED);
 	}
-
 
 	getch();
 	closegraph();
-
-
 }
